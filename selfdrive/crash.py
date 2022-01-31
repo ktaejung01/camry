3a5176a30e41bcfc63d59576ff05bb6f1dc7ccaa
kenny <83075594+ktaejung01@users.noreply.github.com>
2022-01-31 17:30:24 +0900

    Update crash.py

--- selfdrive/crash.py
+++ selfdrive/crash.py
@@ -1,6 +1,9 @@
 """Install exception handler for process crash."""
 from selfdrive.swaglog import cloudlog
 from selfdrive.version import get_version
+import traceback
+import datetime
+
 
 import sentry_sdk
 from sentry_sdk.integrations.threading import ThreadingIntegration
@@ -8,6 +11,14 @@ from sentry_sdk.integrations.threading import ThreadingIntegration
 def capture_exception(*args, **kwargs) -> None:
   cloudlog.error("crash", exc_info=kwargs.get('exc_info', 1))
 
+  try:
+    with open('/data/log/last_exception', 'w') as f:
+      now = datetime.datetime.now()
+      f.write(now.strftime('[%Y-%m-%d %H:%M:%S]') + "\n\n" + str(traceback.format_exc()))
+  except Exception:
+    pass
+
+  
   try:
     sentry_sdk.capture_exception(*args, **kwargs)
     sentry_sdk.flush()  # https://github.com/getsentry/sentry-python/issues/291
