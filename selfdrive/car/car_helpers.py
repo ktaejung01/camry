17deedcc20893b918bb53740db4cff7158b8b08e
kenny <83075594+ktaejung01@users.noreply.github.com>
2022-02-05 21:29:51 +0900

    Update car_helpers.py

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -18,7 +18,7 @@ def get_startup_event(car_recognized, controller_available, fw_seen):
  #   event = EventName.startup
  # else:
 #    event = EventName.startupMaster
-event = EventName.startup
+     event = EventName.startup
 
   if not car_recognized:
     if fw_seen:
